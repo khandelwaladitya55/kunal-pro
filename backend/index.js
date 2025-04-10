e79dc9e8ae4f748bbbc944e9958b30ee7ec8f874
adiyta khandelwal <khandelwaladiyta725@gmail.com>
2025-04-10 13:21:28 +0530

    index.js

--- backend/index.js
+++ backend/index.js
@@ -3,49 +3,43 @@ const mongoose = require("mongoose");
 const categoryRouter = require("./routers/categoryrouter");
 const colorRouter = require("./routers/colorRouter");
 const productRouter = require("./routers/product.router");
-const AdminRouter = require("./routers/adminrouter");
-const cors = require('cors');
 const adminRouter = require("./routers/adminrouter");
+const cors = require("cors");
 const cookieParser = require("cookie-parser");
-require("dotenv").config();
 const jwt = require("jsonwebtoken");
-
-
-
+require("dotenv").config();
 
 const app = express();
+
+// Middleware
 app.use(cookieParser());
 app.use(express.static("./public"));
 app.use(express.json());
-app.use(cors({origin:'http://localhost:3000',credentials:true}));
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true
+}));
 
-app.use("/category",categoryRouter);
-app.use("/color",colorRouter);
-app.use("/product",productRouter);  
-app.use("/admin",adminRouter);
-app.get("/get-cookie",
-    (req,res)=>
-    {
-return res.json({...req.cookies})
-    }
-)
+// Routes
+app.use("/category", categoryRouter);
+app.use("/color", colorRouter);
+app.use("/product", productRouter);
+app.use("/admin", adminRouter);
 
-mongoose.connect("mongodb://localhost:27017/",{dbName:"wsjp-69"})
-.then(
-    ()=>
-    {
-        app.listen(5000,()=>
-            {
-                console.log("server is running on port 5000");
-            })
-    }
-).catch(
-    ()=>
-    {
-        console.log("unable to connect to the database");
-    }
-)
+// Test Route to get cookies
+app.get("/get-cookie", (req, res) => {
+    return res.json({ ...req.cookies });
+});
 
-
-
-
+// Database connection and server start
+mongoose.connect("mongodb://localhost:27017/", {
+    dbName: "wsjp-69"
+})
+.then(() => {
+    app.listen(5000, () => {
+        console.log("Server is running on port 5000");
+    });
+})
+.catch((err) => {
+    console.error("Unable to connect to the database", err);
+});
